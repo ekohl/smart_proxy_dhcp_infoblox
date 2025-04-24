ekohl/smart_proxy_dhcp_infoblox

commit f87a1a83f2a3e3e8cad2c241b56695bd99c85c1a

Fixes #21025 - support for non-default dns and network views.

lib/smart_proxy_dhcp_infoblox/fixed_address_crud.rb
@@ -2,14 +2,18 @@ require 'smart_proxy_dhcp_infoblox/common_crud'
 
 module ::Proxy::DHCP::Infoblox
   class FixedAddressCRUD < CommonCRUD
-    def initialize(connection)
+    attr_reader :network_view
+
+    def initialize(connection, network_view)
       @memoized_hosts = []
       @memoized_condition = nil
-      super
+      @network_view = network_view
+      super(connection)
     end
 
     def all_hosts(subnet_address)
-      network = ::Infoblox::Fixedaddress.find(@connection, 'network' => subnet_address, '_max_results' => 2147483646) #2**(32-cidr_to_i(subnet_address)))
+      network = ::Infoblox::Fixedaddress.find(@connection, 'network' => subnet_address, 'network_view' => network_view,
+                                              '_max_results' => 2147483646) #2**(32-cidr_to_i(subnet_address)))
       network.map {|h| build_reservation(h.name, h, subnet_address)}.compact
     end
 
@@ -32,9 +36,10 @@ module ::Proxy::DHCP::Infoblox
     end
 
     def find_hosts(condition, max_results = 1)
-      return @memoized_hosts if (!@memoized_hosts.empty? && @memoized_condition == condition)
+      return @memoized_hosts if (!@memoized_hosts.empty? && @memoized_condition = condition)
       @memoized_condition = condition
-      @memoized_hosts = ::Infoblox::Fixedaddress.find(@connection, condition.merge('_max_results' => max_results))
+      @memoized_hosts = ::Infoblox::Fixedaddress.find(@connection, condition.merge('_max_results' => max_results,
+                                                                                   'network_view' => network_view))
     end
 
     def find_host_and_name_by_ip(ip_address)
@@ -51,6 +56,7 @@ module ::Proxy::DHCP::Infoblox
       host.use_nextserver = true
       host.bootfile = options[:filename]
       host.use_bootfile = true
+      host.network_view = network_view
       host
     end
   end

config/dhcp_infoblox.yml.example
@@ -8,10 +8,13 @@
 :username: "infoblox"
 :password: "infoblox"
 #
-# Record type to manage: can be "host" or "fixed_address"
+# Record type to manage: can be "host" or "fixedaddress"
 #
 :record_type: 'host'
 #
 # Use  pre-definded ranges in networks to find available IP's
 #
 :use_ranges: false
+
+#:dns_view: 'non-default'
+#:network_view: 'another-non-default'

lib/smart_proxy_dhcp_infoblox/dhcp_infoblox_main.rb
@@ -7,13 +7,14 @@ module Proxy::DHCP::Infoblox
     include Proxy::Util
     include IpAddressArithmetic
 
-    attr_reader :connection, :crud, :restart_grid, :unused_ips
+    attr_reader :connection, :crud, :restart_grid, :unused_ips, :network_view
 
-    def initialize(connection, crud, restart_grid, unused_ips, managed_subnets)
+    def initialize(connection, crud, restart_grid, unused_ips, managed_subnets, network_view)
       @connection = connection
       @crud = crud
       @restart_grid = restart_grid
       @unused_ips = unused_ips
+      @network_view = network_view
       super('infoblox', managed_subnets, nil)
     end
 
@@ -76,7 +77,8 @@ module Proxy::DHCP::Infoblox
     end
 
     def find_network(network_address)
-      network = ::Infoblox::Network.find(connection, 'network' => network_address, '_max_results' => 1).first
+      network = ::Infoblox::Network.find(connection, 'network' => network_address, 'network_view' => network_view,
+                                         '_max_results' => 1).first
       raise "Subnet #{network_address} not found" if network.nil?
       network
     end

lib/smart_proxy_dhcp_infoblox/dhcp_infoblox_plugin.rb
@@ -2,7 +2,7 @@ module Proxy::DHCP::Infoblox
   class Plugin < ::Proxy::Provider
     plugin :dhcp_infoblox, ::Proxy::DHCP::Infoblox::VERSION
 
-    default_settings :record_type => 'host', :range => false
+    default_settings :record_type => 'host', :range => false, :dns_view => "default", :network_view => "default"
     validate_presence :username, :password
 
     requires :dhcp, '>= 1.13'

lib/smart_proxy_dhcp_infoblox/host_ipv4_address_crud.rb
@@ -3,10 +3,13 @@ require 'smart_proxy_dhcp_infoblox/network_address_range_regex_generator'
 
 module ::Proxy::DHCP::Infoblox
   class HostIpv4AddressCRUD < CommonCRUD
-    def initialize(connection)
+    attr_reader :dns_view
+
+    def initialize(connection, dns_view)
       @memoized_hosts = []
       @memoized_condition = nil
-      super
+      @dns_view = dns_view
+      super(connection)
     end
 
     def all_hosts(subnet_address)
@@ -15,6 +18,7 @@ module ::Proxy::DHCP::Infoblox
       hosts = ::Infoblox::Host.find(
           @connection,
           'ipv4addr~' => address_range_regex,
+          'view' => dns_view,
           '_max_results' => 2147483646)
 
       ip_addr_matcher = Regexp.new(address_range_regex) # pre-compile the regex
@@ -47,7 +51,8 @@ module ::Proxy::DHCP::Infoblox
     def find_hosts(condition, max_results = 1)
       return @memoized_hosts if (!@memoized_hosts.empty? && @memoized_condition == condition)
       @memoized_condition = condition
-      @memoized_hosts = ::Infoblox::Host.find(@connection, condition.merge('_max_results' => max_results))
+      @memoized_hosts = ::Infoblox::Host.find(@connection, condition.merge('view' => dns_view,
+                                                                           '_max_results' => max_results))
     end
 
     def build_host(options)
@@ -60,6 +65,7 @@ module ::Proxy::DHCP::Infoblox
       host_addr.use_nextserver = true
       host_addr.bootfile = options[:filename]
       host_addr.use_bootfile = true
+      host.view = dns_view
       host
     end
   end

lib/smart_proxy_dhcp_infoblox/plugin_configuration.rb
@@ -20,9 +20,16 @@ module Proxy::DHCP::Infoblox
                                 end)
 
 
-      c.dependency :unused_ips, lambda { ::Proxy::DHCP::Infoblox::UnusedIps.new(c.get_dependency(:connection), settings[:use_ranges])}
-      c.dependency :host_ipv4_crud, lambda { ::Proxy::DHCP::Infoblox::HostIpv4AddressCRUD.new(c.get_dependency(:connection))}
-      c.dependency :fixed_address_crud, lambda { ::Proxy::DHCP::Infoblox::FixedAddressCRUD.new(c.get_dependency(:connection))}
+      c.dependency :unused_ips, (lambda do
+        ::Proxy::DHCP::Infoblox::UnusedIps.new(c.get_dependency(:connection), settings[:use_ranges],
+                                             settings[:network_view])
+      end)
+      c.dependency :host_ipv4_crud, (lambda do
+        ::Proxy::DHCP::Infoblox::HostIpv4AddressCRUD.new(c.get_dependency(:connection), settings[:dns_view])
+      end)
+      c.dependency :fixed_address_crud, (lambda do
+        ::Proxy::DHCP::Infoblox::FixedAddressCRUD.new(c.get_dependency(:connection), settings[:network_view])
+      end)
       c.dependency :grid_restart, lambda { ::Proxy::DHCP::Infoblox::GridRestart.new(c.get_dependency(:connection))}
       c.dependency :dhcp_provider, (lambda do
                                       ::Proxy::DHCP::Infoblox::Provider.new(
@@ -30,8 +37,10 @@ module Proxy::DHCP::Infoblox
                                         settings[:record_type] == 'host' ? c.get_dependency(:host_ipv4_crud) : c.get_dependency(:fixed_address_crud),
                                         c.get_dependency(:grid_restart),
                                         c.get_dependency(:unused_ips),
-                                        settings[:subnets])
+                                        settings[:subnets],
+                                          settings[:network_view])
                                       end)
     end
   end
 end
+

lib/smart_proxy_dhcp_infoblox/unused_ips.rb
@@ -4,12 +4,13 @@ require 'smart_proxy_dhcp_infoblox/ip_address_arithmetic'
 module Proxy::DHCP::Infoblox
   class UnusedIps
     include IpAddressArithmetic
-    attr_reader :connection, :use_ranges
+    attr_reader :connection, :use_ranges, :network_view
 
-    def initialize(connection, use_ranges)
+    def initialize(connection, use_ranges, network_view)
       @connection = connection
       @use_ranges = use_ranges
       @memoized_network = nil
+      @network_view = network_view
     end
 
     def unused_ip(network_address, from_ip_address, to_ip_address)
@@ -31,7 +32,7 @@ module Proxy::DHCP::Infoblox
     end
 
     def find_range(network_address, from, to)
-      ranges = ::Infoblox::Range.find(@connection, 'network' => network_address)
+      ranges = ::Infoblox::Range.find(@connection, 'network' => network_address, 'network_view' => network_view)
       range = (from.nil? || to.nil?) ? ranges.first : ranges.find {|r| r.start_addr == from && r.end_addr == to}
       raise "No Ranges found for #{network_address} network" if range.nil?
       range
@@ -40,7 +41,8 @@ module Proxy::DHCP::Infoblox
     def find_network(network_address)
       return @memoized_network if !@memoized_network.nil? && @memoized_address == network_address
       @memoized_address = network_address
-      @memoized_network = ::Infoblox::Network.find(@connection, 'network' => network_address, '_max_results' => 1).first
+      @memoized_network = ::Infoblox::Network.find(@connection, 'network' => network_address, 'network_view' => network_view,
+                                                   '_max_results' => 1).first
       raise "Subnet #{network_address} not found" if @memoized_network.nil?
       @memoized_network
     end

test/host_and_fixedaddress_crud_test.rb
@@ -7,27 +7,27 @@ require 'smart_proxy_dhcp_infoblox/host_ipv4_address_crud'
 
 module CommoncrudTests
   def test_find_record_using_ip
-    @entity.expects(:find).with(@connection, 'ipv4addr' => '192.168.42.1', '_max_results' => 1).returns([@host])
+    @entity.expects(:find).with(@connection, search_condition('ipv4addr' => '192.168.42.1', '_max_results' => 1)).returns([@host])
     assert_equal @reservation, @crud.find_record_by_ip('192.168.42.0/24', '192.168.42.1')
   end
 
   def test_find_record_using_ip_returns_nil_if_record_not_found
-    @entity.expects(:find).with(@connection, 'ipv4addr' => '192.168.42.1', '_max_results' => 1).returns([])
+    @entity.expects(:find).with(@connection, search_condition('ipv4addr' => '192.168.42.1', '_max_results' => 1)).returns([])
     assert_nil @crud.find_record_by_ip('192.168.42.0/24', '192.168.42.1')
   end
 
   def test_find_records_using_ip_returns_empty_arrays_if_records_not_found
-    @entity.expects(:find).with(@connection, 'ipv4addr' => '192.168.42.1', '_max_results' => 2147483646).returns([])
+    @entity.expects(:find).with(@connection, search_condition('ipv4addr' => '192.168.42.1', '_max_results' => 2147483646)).returns([])
     assert @crud.find_records_by_ip('192.168.42.0/24', '192.168.42.1').empty?
   end
 
   def test_find_record_using_mac
-    @entity.expects(:find).with(@connection, 'mac' => '00:01:02:03:05:06', '_max_results' => 1).returns([@host])
+    @entity.expects(:find).with(@connection, search_condition('mac' => '00:01:02:03:05:06', '_max_results' => 1)).returns([@host])
     assert_equal @reservation, @crud.find_record_by_mac('192.168.42.0/24', '00:01:02:03:05:06')
   end
 
   def test_find_record_using_mac_returns_nil_if_record_not_found
-    @entity.expects(:find).with(@connection, 'mac' => '00:01:02:03:05:06', '_max_results' => 1).returns([])
+    @entity.expects(:find).with(@connection, search_condition('mac' => '00:01:02:03:05:06', '_max_results' => 1)).returns([])
     assert_nil @crud.find_record_by_mac('192.168.42.0/24', '00:01:02:03:05:06')
   end
 
@@ -81,7 +81,8 @@ class HostCrudTest < Test::Unit::TestCase
 
   def setup
     @connection = Object.new
-    @crud = ::Proxy::DHCP::Infoblox::HostIpv4AddressCRUD.new(@connection)
+    @view = "something"
+    @crud = ::Proxy::DHCP::Infoblox::HostIpv4AddressCRUD.new(@connection, @view)
 
     @entity = ::Infoblox::Host
 
@@ -94,10 +95,12 @@ class HostCrudTest < Test::Unit::TestCase
 
     @host = ::Infoblox::Host.new(
         :name => @hostname,
+        :view => @view,
         :ipv4addrs => [{:ipv4addr => @ip, :mac => @mac, :nextserver => @nextserver, :use_nextserver => true,
                         :bootfile => @filename, :use_bootfile => true, :configure_for_dhcp => true}])
     @host1 = ::Infoblox::Host.new(
         :name => 'another.test.com',
+        :view => @view,
         :ipv4addrs => [{:ipv4addr => @ip, :mac => '00:01:02:03:05:07', :nextserver => @nextserver, :use_nextserver => true,
                         :bootfile => @filename, :use_bootfile => true, :configure_for_dhcp => true}])
 
@@ -107,7 +110,8 @@ class HostCrudTest < Test::Unit::TestCase
   end
 
   def test_all_hosts
-    ::Infoblox::Host.expects(:find).with(@connection, 'ipv4addr~' => '192\.168\.42\..+', '_max_results' => 2147483646).returns([@host])
+    ::Infoblox::Host.expects(:find).with(@connection, 'ipv4addr~' => '192\.168\.42\..+', 'view' => @view,
+                                         '_max_results' => 2147483646).returns([@host])
     assert_equal @reservation, @crud.all_hosts('192.168.42.0/24').first
   end
 
@@ -128,22 +132,28 @@ class HostCrudTest < Test::Unit::TestCase
     assert_equal expected_addr.bootfile, actual_addr.bootfile
     assert actual_addr.use_bootfile
     assert actual_addr.configure_for_dhcp
+    assert_equal @view, built.view
   end
 
   def test_add_record_with_collision
     @crud.expects(:build_host).with(:ip => @ip, :mac => @mac, :hostname => @hostname).returns(@host)
     @host.expects(:post).raises(Infoblox::Error.new("IB.Data.Conflict"))
     @crud.expects(:find_hosts).with('ipv4addr' => @ip).returns(
-        [::Infoblox::Host.new(:name => @hostname, :ipv4addrs => [{:ipv4addr => @ip, :mac => '11:22:33:44:55:66', :configure_for_dhcp => true}])])
+        [::Infoblox::Host.new(:name => @hostname, :ipv4addrs => [{:ipv4addr => @ip, :mac => '11:22:33:44:55:66"', :configure_for_dhcp => true}])])
 
     assert_raises(Proxy::DHCP::Collision) { @crud.add_record(:ip => @ip, :mac => @mac, :hostname => @hostname) }
+  end
+
+  def search_condition(a_hash)
+    {'view' => @view}.merge(a_hash)
   end
 end
 
 class FixedaddressCrudTest < Test::Unit::TestCase
   def setup
     @connection = Object.new
-    @crud = ::Proxy::DHCP::Infoblox::FixedAddressCRUD.new(@connection)
+    @network_view = "something"
+    @crud = ::Proxy::DHCP::Infoblox::FixedAddressCRUD.new(@connection, @network_view)
 
     @entity = ::Infoblox::Fixedaddress
 
@@ -156,17 +166,18 @@ class FixedaddressCrudTest < Test::Unit::TestCase
 
     @host = ::Infoblox::Fixedaddress.new(
         :name => @hostname,
-        :ipv4addr => @ip, :mac => @mac) # :ipv4addr => @ip, :mac => @mac) #
+        :ipv4addr => @ip, :mac => @mac, :network_view => @network_view)
     @host1 = ::Infoblox::Fixedaddress.new(
         :name => 'another.test.com',
-        :ipv4addr => @ip, :mac => '00:01:02:03:05:07')
+        :ipv4addr => @ip, :mac => '00:01:02:03:05:07', :network_view => @network_view)
 
     @reservation = ::Proxy::DHCP::Reservation.new(@hostname, @ip, @mac, ::Proxy::DHCP::Subnet.new(@subnet_ip, '255.255.255.0'),
                                                   :deleteable => true, :hostname => @hostname)
   end
 
   def test_all_hosts
-    ::Infoblox::Fixedaddress.expects(:find).with(@connection, 'network' => '192.168.42.0/24', '_max_results' => 2147483646).returns([@host])
+    ::Infoblox::Fixedaddress.expects(:find).with(@connection, 'network' => '192.168.42.0/24', 'network_view' => @network_view,
+                                                 '_max_results' => 2147483646).returns([@host])
     assert_equal @reservation, @crud.all_hosts('192.168.42.0/24').first
   end
 
@@ -181,14 +192,20 @@ class FixedaddressCrudTest < Test::Unit::TestCase
     assert built.use_nextserver
     assert_equal @filename, built.bootfile
     assert built.use_bootfile
+    assert_equal @network_view, built.network_view
   end
 
   def test_add_record_with_collision
     @crud.expects(:build_host).with(:ip => @ip, :mac => @mac, :hostname => @hostname).returns(@host)
     @host.expects(:post).raises(Infoblox::Error.new("IB.Data.Conflict"))
     @crud.expects(:find_hosts).with('ipv4addr' => @ip).returns(
-        [::Infoblox::Fixedaddress.new(:name => @hostname, :ipv4addr => @ip, :mac => '11:22:33:44:55:66')])
+        [::Infoblox::Fixedaddress.new(:name => @hostname, :ipv4addr => @ip, :mac => '11:22:33:44:55:66',
+                                      :network_view => @network_view)])
 
     assert_raises(Proxy::DHCP::Collision) { @crud.add_record(:ip => @ip, :mac => @mac, :hostname => @hostname) }
+  end
+
+  def search_condition(a_hash)
+    {'network_view' => @network_view}.merge(a_hash)
   end
 end

commit 12ae55aaecc1144588aee8c97526d83c3b0aaf96

Fix logic bug in find_hosts caching
The single = resulted in the value being assigned. If it's non-nil then
it will also be true, even though the code suggests it was intended as a
condition. In f87a1a83f2a3e3e8cad2c241b56695bd99c85c1a it was changed,
but it looks like that may have been accidental.

Fixes: f87a1a83f2a3 ("Fixes #21025 - support for non-default dns and network views.")

lib/smart_proxy_dhcp_infoblox/fixed_address_crud.rb
@@ -65,7 +65,7 @@ module ::Proxy::DHCP::Infoblox
     end
 
     def find_hosts(condition, max_results = 1)
-      return @memoized_hosts if (!@memoized_hosts.empty? && @memoized_condition = condition)
+      return @memoized_hosts if !@memoized_hosts.empty? && @memoized_condition == condition
 
       @memoized_condition = condition
       @memoized_hosts = ::Infoblox::Fixedaddress.find(@connection, condition.merge('_max_results' => max_results,